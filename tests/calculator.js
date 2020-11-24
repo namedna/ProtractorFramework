16b098b6a1cbf781af992d2816a242669bafbe26
namedna <dbaoanh.1512@gmail.com>
2020-11-24 14:27:29 +0700

    my first project commit

---
+++ tests/calculator.js
@@ -0,0 +1,37 @@
+let homepage = require("../pages/homepage");
+
+describe("Demo Calculator Tests", function () {
+  it("Additon test", function () {
+    //browser.get("https://juliemr.github.io/protractor-demo/");
+    homepage.get("https://juliemr.github.io/protractor-demo/");
+
+    //element(by.model("first")).sendKeys("23");
+    homepage.enterFirstNumber("223");
+
+    //element(by.model("second")).sendKeys("11");
+    homepage.enterSecondNumber("11");
+
+    //element(by.css('[ng-click="doAddition()"]')).click();
+    homepage.goButton();
+
+    /* let result = element(by.cssContainingText(".ng-binding", "34"));
+    expect(result.getText()).toEqual("34"); */
+    homepage.verifyResult("234");
+
+    browser.sleep(3000);
+  });
+
+  it("Subtraction test", function () {
+    homepage.get("https://juliemr.github.io/protractor-demo/");
+
+    homepage.enterFirstNumber("223");
+
+    homepage.enterSecondNumber("11");
+
+    homepage.goButton1();
+
+    homepage.verifyResult("234");
+
+    browser.sleep(3000);
+  });
+});
---
+++ tests/hello.js
@@ -0,0 +1,1 @@
+console.log("Hello, it's me. I'm Na.Hiiii, i'm new intern !!!");
